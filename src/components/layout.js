3d449a51b38996fc7cc173d3963217475933b86d
Florian Gächter <flo@frontify.com>
2020-02-13 09:05:36 +0100

    feat: add social links to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types"
 import React from "react"
 import { ReactComponent as Gatsby } from "../images/gatsby.svg"
 import { ReactComponent as Netlify } from "../images/netlify.svg"
+import { ReactComponent as Email } from "../images/email.svg"
+import { ReactComponent as RSS } from "../images/rss.svg"
+import { ReactComponent as Github } from "../images/github.svg"
+import { ReactComponent as Twitter } from "../images/twitter.svg"
 import Header from "./header"
 
 const Layout = ({ children }) => {
@@ -20,16 +24,66 @@ const Layout = ({ children }) => {
     <div className="container flex flex-col min-h-screen">
       <Header siteTitle={data.site.siteMetadata.title} />
       <main className="flex-grow">{children}</main>
-      <footer className="flex items-center text-sm my-12">
-        © {new Date().getFullYear()}, Built with
-        <a aria-label="Gatsby" href="https://www.gatsbyjs.org/">
-          <Gatsby className="h-4 ml-1" />
-        </a>
-        , running on
-        <a aria-label="Netlify" href="https://www.netlify.com/">
-          <Netlify className="h-4 ml-1" />
-        </a>
-        .
+      <footer className="sm:flex items-center justify-between text-sm my-12">
+        <div className="flex items-center mb-4 sm:mb-0">
+          © {new Date().getFullYear()}, Built with
+          <a
+            aria-label="Gatsby"
+            title="Gatsby"
+            href="https://www.gatsbyjs.org/"
+            rel="noreferrer noopener"
+          >
+            <Gatsby className="h-4 ml-1" />
+          </a>
+          , running on
+          <a
+            aria-label="Netlify"
+            title="Netlify"
+            href="https://www.netlify.com/"
+            rel="noreferrer noopener"
+          >
+            <Netlify className="h-4 ml-1" />
+          </a>
+          .
+        </div>
+        <div className="flex items-center -ml-4 sm:-ml-0 -mr-4">
+          <a
+            aria-label="RSS"
+            title="RSS"
+            className="mx-4"
+            href="/rss.xml"
+            rel="noreferrer noopener"
+          >
+            <RSS className="h-4" />
+          </a>
+          <a
+            aria-label="E-mail"
+            title="E-mail"
+            className="mx-4"
+            href="mailto:florian.gaechter@gmail.com"
+            rel="noreferrer noopener"
+          >
+            <Email className="h-4" />
+          </a>
+          <a
+            aria-label="Github"
+            title="Github"
+            className="mx-4"
+            href="https://github.com/floriangaechter/"
+            rel="noreferrer noopener"
+          >
+            <Github className="h-4" />
+          </a>
+          <a
+            aria-label="Twitter"
+            title="Twitter"
+            className="mx-4"
+            href="https://twitter.com/neither1nor0"
+            rel="noreferrer noopener"
+          >
+            <Twitter className="h-4" />
+          </a>
+        </div>
       </footer>
     </div>
   )
