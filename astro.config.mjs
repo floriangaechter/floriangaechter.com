5158e98f99314b2f650cbe7317c52f48284e910f
Florian Gächter <florian.gaechter@frontify.com>
2022-12-06 14:42:05 +0100

    fix: remove unused astro/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
 import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), image(), mdx(), alpinejs(), astroImageTools],
+	integrations: [tailwind(), mdx(), alpinejs(), astroImageTools],
 	site: "https://www.floriangaechter.com/",
 	trailingSlash: "always",
 	markdown: {
