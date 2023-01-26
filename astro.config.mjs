fa092e97b87d235aa5194c02b95326aa81269b5d
Florian Gächter <flo@gaechter.xyz>
2023-01-26 12:22:35 +0100

    chore: fix indenting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import alpinejs from "@astrojs/alpinejs";
 import { astroImageTools } from "astro-imagetools";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), alpinejs(), astroImageTools, sitemap()],
-  site: "https://www.floriangaechter.com/",
-  trailingSlash: "always",
-  markdown: {
-    shikiConfig: {
-      theme: "nord"
-    }
-  }
+	integrations: [tailwind(), mdx(), alpinejs(), astroImageTools, sitemap()],
+	site: "https://www.floriangaechter.com/",
+	trailingSlash: "always",
+	markdown: {
+		shikiConfig: {
+			theme: "nord",
+		},
+	},
 });
