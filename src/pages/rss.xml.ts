f6103ccdc27705e33303a734bce07cb5ddfa67d6
Florian Gächter <flo@gaechter.xyz>
2023-01-26 12:22:48 +0100

    fix: update rss items collection

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,14 @@
-import rss from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import type { APIContext } from "astro";
 
-export const get = () =>
-	rss({
-		title: 'Florian Gächters’s Blog',
+export const get = async (context: APIContext) => {
+	return rss({
+		title: "Florian Gächters’s Blog",
 		description:
-			'Personal blog of Florian Gächter. These are a couple of things I’ve come across so far…',
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./posts/**/*.md'),
+			"Personal blog of Florian Gächter. These are a couple of things I’ve come across so far…",
+		site: context.site,
+		items: await pagesGlobToRssItems(import.meta.glob("./posts/**/*.{md,mdx}")),
 		customData: `<language>en-us</language>`,
-		stylesheet: '/rss/pretty-feed-v3.xsl',
+		stylesheet: "/rss/pretty-feed-v3.xsl",
 	});
+};
