95e2faba195fbaab347fe0c10c595e23503169e5
Florian Gächter <flo@gaechter.xyz>
2023-12-08 10:26:54 +0100

    fix: show post content in rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context: { site: string; }) {
 	const posts = await getCollection("posts");
@@ -8,12 +11,13 @@ export async function GET(context: { site: string; }) {
 		title: "Florian Gächter’s Blog",
 		description: "Developer Advocate at Frontify sharing tips, tricks, and learnings about technology and software development. Based in Switzerland.",
 		site: context.site,
+		stylesheet: "/rss/styles.xsl",
 		items: posts.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.pubDate,
 			description: post.data.description,
 			link: `/posts/${post.slug}`,
+			content: sanitizeHtml(parser.render(post.body)),
 		})),
 	});
 }
-
