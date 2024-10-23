ff84788ba0d2a62ed970eab2de319edf1ceffe7c
Florian Gächter <flo@gaechter.xyz>
2024-10-23 14:10:36 +0200

    fix: properly render mdx in rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,23 +1,30 @@
 import rss from "@astrojs/rss";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
+import { loadRenderers } from "astro:container";
 import { getCollection } from "astro:content";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+import { type APIContext } from "astro";
 
-export async function GET(context: { site: string; }) {
-  const posts = await getCollection("posts");
+export async function GET(context: APIContext) {
+	const renderers = await loadRenderers([getMDXRenderer()]);
+	const container = await AstroContainer.create({ renderers });
+	const posts = await getCollection("posts");
+	const items = [];
 
-  return rss({
-    title: "Florian Gächter’s Blog",
-    description: "Developer Advocate at Frontify sharing tips, tricks, and learnings about technology and software development. Based in Switzerland.",
-    site: context.site,
-    stylesheet: "/rss/styles.xsl",
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/posts/${post.slug}`,
-      content: sanitizeHtml(parser.render(post.body)),
-    })),
-  });
+	for (const post of posts) {
+		const { Content } = await post.render();
+		const content = await container.renderToString(Content);
+		const link = new URL(`/posts/${post.slug}`, context.url.origin).toString();
+
+		items.push({ ...post.data, link, content });
+	}
+
+	return rss({
+		title: "Florian Gächter’s Blog",
+		description:
+			"Developer Advocate at Frontify sharing tips, tricks, and learnings about technology and software development. Based in Switzerland.",
+		site: context.site,
+		stylesheet: "/rss/styles.xsl",
+		items,
+	});
 }
