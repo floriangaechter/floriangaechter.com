3bd4abe3148bfadb5b4eb1ece5fcbb197c02b20c
Florian Gächter <flo@gaechter.xyz>
2025-03-04 16:28:07 +0100

    wip: several updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,33 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import flexokiDark from "./flexoki-dark.json";
+import flexokiLight from "./flexoki-light.json";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.floriangaechter.com",
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+
   markdown: {
     shikiConfig: {
-      theme: {
-        name: "flexoki-dark",
-        type: "dark",
-        settings: flexokiDark.tokenColors,
+      themes: {
+        dark: {
+          name: "flexoki-dark",
+          type: "dark",
+          settings: flexokiDark.tokenColors,
+        },
+        light: {
+          name: "flexoki-light",
+          type: "light",
+          settings: flexokiLight.tokenColors,
+        },
       },
     },
   },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
+
